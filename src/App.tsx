ec3ed1c52f4165e7183b7b9610f5c4420f6ef9a2
bhlong <bhlong3112@gmail.com>
2022-10-05 21:15:47 +0700

    bai tap lam thu

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  // const MAX_NUMBER = 100;
+  //let age_person = 18;
+  //let nameCar = { name: "value" };
+  // let styles = { color: " red", fontSize: 18 };
+
+  return (
+    <>
+      <h1 className="header"> The Pulpit Rock </h1>
+    </>
+  );
+}
+
+export default App;
