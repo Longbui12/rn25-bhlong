5ff58c1fd8313d7b90c4b6735b52bacaad2350cd
bhlong <bhlong3112@gmail.com>
2022-10-07 15:56:51 +0700

    bai tap ve nha

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   // const MAX_NUMBER = 100;
@@ -10,7 +11,66 @@ function App() {
 
   return (
     <>
-      <h1 className="header"> The Pulpit Rock </h1>
+      <h1
+        style={{
+          width: "100%",
+          height: "60px",
+          textAlign: "center",
+          backgroundColor: "lightgray",
+        }}
+      >
+        The Pulpit Rock
+      </h1>
+      <div className="row" style={{height:"700px"}}>
+        <div
+          className="col-md-2"
+          style={{
+            textDecoration: "underline",
+            backgroundColor: "lightpink",
+            height: "150px",
+          }}
+        >
+          <p>The first</p>
+          <p>The Second</p>
+          <p>The third</p>
+          <p>The fourth</p>
+        </div>
+        <div className="col-md-7">
+          <h1> The Topic </h1>
+          <p>
+            The United States on Thursday imposed sanctions on a Myanmar
+            businessman and two others involved in procuring Russian-made
+            weapons from Belarus for the junta that seized power in the
+            Southeast Asian country early last year, Secretary of State Antony
+            Blinken said. The military staged a coup in February 2021, detaining
+            democratic leaders including Nobel laureate Aung San Suu Kyi, then
+            violently suppressed protests, sparking a spiraling conflict.
+            <br />
+          </p>
+        </div>
+        <div
+          className="col-md-3 bg-primary"
+          style={{ height: "400px", marginTop: "5px", marginBottom: "2px" }}
+        >
+          <h2>What's your name?</h2>
+          <p>My name is...</p>
+          <h3>Where are you from?</h3>
+          <p>I am come from...</p>
+          <h4>How do you feel today?</h4>
+          <p>I'm fill so...</p>
+        </div>
+      </div>
+      <h1
+        className="footer"
+        style={{
+          height: "60px",
+          textAlign: "center",
+          backgroundColor: "lightgray",
+          marginTop: "20px",
+        }}
+      >
+        This Is The footer
+      </h1>
     </>
   );
 }
